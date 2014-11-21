65b017ca72a0c88871059f412080c9efec09e541
Xiaoxiong Xing <xingxiaoxiong@frontier.hokudai.ac.jp>
2014-11-21 21:38:08 +0900

    Set simple user permissions

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,5 +1,6 @@
 class HousesController < ApplicationController
   before_action :set_house, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /houses
   # GET /houses.json
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,7 @@ class ReviewsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_review, only: [:edit, :update, :destroy]
   before_action :set_house
+  before_action :check_user, only: [:edit, :update, :destroy]  
 
 
   # GET /reviews/new
@@ -38,7 +39,7 @@ class ReviewsController < ApplicationController
   def update
     respond_to do |format|
       if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+        format.html { redirect_to house_url(@house), notice: 'Review was successfully updated.' }
         format.json { render :show, status: :ok, location: @review }
       else
         format.html { render :edit }
@@ -52,7 +53,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_to house_url(@house), notice: 'Review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -65,6 +66,12 @@ class ReviewsController < ApplicationController
 
     def set_house
       @house = House.find(params[:house_id])
+    end
+
+    def check_user
+      unless (@review.user == current_user) || (current_user.admin?)
+        redirect_to root_url, alert: "Sorry, this review belongs to someone else"
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
